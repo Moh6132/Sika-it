14c69aab7d6ab1587c57282afd6d454df59fb5f0
Moh6132 <99084820+Moh6132@users.noreply.github.com>
2026-03-27 19:37:22 -0700

    Implement carousel functionality with navigation

--- script.js
+++ script.js
@@ -57,3 +57,29 @@ window.addEventListener('scroll', () => {
 
     cube.style.transform = `rotateX(${rx}deg) rotateY(${ry}deg)`;
 });
+let activeIndex = 0;
+const cards = document.querySelectorAll('.card-container');
+
+function updateCarousel() {
+    cards.forEach((card, i) => {
+        const offset = i - activeIndex;
+        card.style.setProperty('--offset', offset);
+        card.style.setProperty('--abs-offset', Math.abs(offset));
+        card.style.setProperty('--direction', offset > 0 ? 1 : offset < 0 ? -1 : 0);
+        card.style.setProperty('--active', i === activeIndex ? 1 : 0);
+    });
+}
+
+function moveLeft() {
+    if (activeIndex > 0) {
+        activeIndex--;
+        updateCarousel();
+    }
+}
+
+function moveRight() {
+    if (activeIndex < cards.length - 1) {
+        activeIndex++;
+        updateCarousel();
+    }
+}
